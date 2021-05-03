77a83f4d88932de6558b996a324c4f00e4e6fc89
Fernando Marca Magalhães <59258828+fernandomarca@users.noreply.github.com>
2021-05-03 20:37:05 -0300

    configuration typeorm and postgres container

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express, { json } from "express";
-import { router } from "./routes";
 import swaggerUi from "swagger-ui-express";
+import { router } from "./routes";
 import swaggerFile from "./swagger.json";
+
+import "./database";
 
 const app = express();
 
