dbc8807a7531cfc87aecd92e12c16d6296d7a4ef
Fernando Marca Magalhães <59258828+fernandomarca@users.noreply.github.com>
2021-05-08 22:21:46 -0300

    update avatar user

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -29,6 +29,9 @@ export async function ensureAuthenticated(
     if (!user) {
       throw new AppError("User does not exists!", 401);
     }
+    request.user = {
+      id: user_id,
+    };
     next();
   } catch (error) {
     throw new AppError("Invalid Token!", 401);
