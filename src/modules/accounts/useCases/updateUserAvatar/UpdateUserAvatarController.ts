3cd97de68cfae013e44e98ca276a407dd8da4710
Fernando Marca Magalhães <59258828+fernandomarca@users.noreply.github.com>
2021-05-08 22:37:49 -0300

    delete user avatar picture

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -4,7 +4,7 @@ import { UpdateUserAvatarUseCase } from "./UpdateUserAvatarUseCase";
 
 class UpdateUserAvatarController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { id } = request.user;
+    const { id } = request.user; // vem do ensureAuthenticated request.user
     const avatar_file = request.file.filename;
     const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
 
--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,11 +1,11 @@
 import { inject, injectable } from "tsyringe";
+import { deleteFile } from "../../../../utils/file";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
   user_id: string;
   avatar_file: string;
 }
-
 @injectable()
 class UpdateUserAvatarUseCase {
   constructor(
@@ -15,6 +15,9 @@ class UpdateUserAvatarUseCase {
 
   async execute({ user_id, avatar_file }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(user_id);
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }
     user.avatar = avatar_file;
     await this.usersRepository.create(user);
   }
