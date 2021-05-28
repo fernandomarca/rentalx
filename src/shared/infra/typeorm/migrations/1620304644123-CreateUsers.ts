80aa648ee440d1e02d2689bd38b094a26e8acffb
Fernando Marca Magalhães <59258828+fernandomarca@users.noreply.github.com>
2021-05-28 19:34:56 -0300

    migration rentals

--- src/shared/infra/typeorm/migrations/1620304644123-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1620304644123-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1620304644123 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
