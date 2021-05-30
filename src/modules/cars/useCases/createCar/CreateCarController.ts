5ff369fac54c8136910c7a6a98d833f75e607b70
Fernando Marca Magalhães <59258828+fernandomarca@users.noreply.github.com>
2021-05-30 16:00:44 -0300

    testes de integração

--- src/modules/cars/useCases/createCar/CreateCarController.ts
+++ src/modules/cars/useCases/createCar/CreateCarController.ts
@@ -4,6 +4,7 @@ import { CreateCarUseCase } from "./CreateCarUseCase";
 class CreateCarController {
   async handle(request: Request, response: Response): Promise<Response> {
     const {
+      id,
       brand,
       category_id,
       daily_rate,
@@ -16,6 +17,7 @@ class CreateCarController {
     const createCarUseCase = container.resolve(CreateCarUseCase);
 
     const car = await createCarUseCase.execute({
+      id,
       brand,
       category_id,
       daily_rate,
