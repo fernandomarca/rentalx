58532a8517a33521e622bb367ff464f5e33c2612
Fernando Marca Magalhães <59258828+fernandomarca@users.noreply.github.com>
2021-06-03 18:05:49 -0300

    swagger implementacion

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,7 +1,10 @@
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
+  ManyToOne,
   PrimaryColumn,
   UpdateDateColumn,
 } from "typeorm";
@@ -11,6 +14,10 @@ import { v4 as uuidV4 } from "uuid";
 class Rental {
   @PrimaryColumn()
   id: string;
+
+  @ManyToOne(() => Car)
+  @JoinColumn({ name: "car_id" })
+  car: Car;
 
   @Column()
   car_id: string;
@@ -39,6 +46,10 @@ class Rental {
   constructor() {
     if (!this.id) {
       this.id = uuidV4();
+    }
+
+    if (!this.start_date) {
+      this.start_date = new Date();
     }
   }
 }
