6293d8e48267aa273521e7003c15d01a587cc2dc
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2022-01-10 23:39:31 -0300

    add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -9,6 +9,7 @@ import swaggerUi from "swagger-ui-express";
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 import upload from "@config/upload";
+import cors from "cors";
 
 createConnection();
 
@@ -21,7 +22,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar",express.static(`${upload.tmpFolder}/avatar`));
 
 app.use("/cars",express.static(`${upload.tmpFolder}/cars`));
-
+app.use(cors({}));
 app.use(router);
 
 app.use(
--- src/shared/infra/typeorm/seed/admin.ts
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -3,7 +3,7 @@ import { hash } from "bcrypt";
 import createConnection from "../index";
 
 async function create() {
-  const connection = await createConnection("localhost");
+  const connection = await createConnection();
   const id = uuidV4();
   const password = await hash("admin", 8);
 
