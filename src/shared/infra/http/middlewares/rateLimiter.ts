9776989ad80e2305b79390c8ea5f11208af3e3fe
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2022-01-11 22:58:53 -0300

    tsconfig es2017

--- src/shared/infra/http/middlewares/rateLimiter.ts
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -8,14 +8,14 @@ const redisClient = redis.createClient({legacyMode: true,
   socket: {
     host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT),
+    sessionTimeout:20
   },
 });
-
 const limiter = new RateLimiterRedis({
   storeClient:redisClient,
   keyPrefix:"rateLimiter",
   points:10, //10 requests
-  duration:5 // per 1 second by ip
+  duration:5, // per 1 second by ip
 })
 
 export default async function rateLimiter(request:Request,response:Response,next:NextFunction):Promise<void>{
